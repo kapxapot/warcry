6b8625e3837bb3ff6166569e85f7f5a22269cdbe
MSC\sua1 <sua1@europlan.ru>
2018-01-18 12:06:54 +0300

    Pre-refactoring and pre-boilerplate update

--- src/Generators/Entities/Articles.php
+++ src/Generators/Entities/Articles.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Route\Generators\Entities;
+namespace App\Generators\Entities;
 
-use App\Route\Generators\EntityGenerator;
+use App\Generators\PublishableGenerator;
 
-class Articles extends EntityGenerator {
+class Articles extends PublishableGenerator {
 	public function getRules($data, $id = null) {
 		$rules = [
 			'name_ru' => $this->rule('text'),//->regex($cyr("'\(\):\-\.\|,\?!—«»"));
@@ -30,6 +30,29 @@ class Articles extends EntityGenerator {
 		$data['cache'] = null;
 		$data['contents_cache'] = null;
 		
+		$data = $this->publishIfNeeded($data);		
+		
 		return $data;
+	}
+
+	public function afterSave($item, $data) {
+		$this->notify($item, $data);
+	}
+
+	private function notify($item, $data) {
+		if ($this->isJustPublished($item, $data) && $item->announce == 1) {
+			if ($item->cat) {
+				$catObj = $this->db->getCat($item->cat);
+				if ($catObj) {
+					$catName = $catObj['name_en'];
+				}
+			}
+			
+			$url = $this->legacyRouter->article($item->name_en, $catName);
+			$url = $this->legacyRouter->abs($url);
+			
+			$this->telegram->sendMessage('warcry', "Опубликована статья:
+<a href=\"{$url}\">{$item->name_ru}</a>");
+		}
 	}
 }
--- src/Generators/Entities/News.php
+++ src/Generators/Entities/News.php
@@ -1,19 +1,43 @@
 <?php
 
-namespace App\Route\Generators\Entities;
+namespace App\Generators\Entities;
 
-use Warcry\Util\Util;
+use Warcry\Util\Strings;
 
-use App\Route\Generators\EntityGenerator;
+use App\DB\Taggable;
+use App\Generators\PublishableGenerator;
 
-class News extends EntityGenerator {
+class News extends PublishableGenerator {
 	public function beforeSave($data, $id = null) {
 		$data['cache'] = null;
-		
-		if (isset($data['published']) && $data['published'] == 1 && !isset($data['published_at'])) {
-			$data['published_at'] = Util::now();
-		}
+
+		$data = $this->publishIfNeeded($data);		
 
 		return $data;
+	}
+
+	public function afterSave($item, $data) {
+		$this->updateTags($item);
+		$this->notify($item, $data);
+	}
+	
+	private function updateTags($item) {
+		$tags = Strings::toTags($item->tags);
+
+		$this->db->saveTags(Taggable::NEWS, $item->id, $tags);
+	}
+
+	private function notify($item, $data) {
+		if ($this->isJustPublished($item, $data)) {
+			$url = $this->legacyRouter->news($item->id);
+			$url = $this->legacyRouter->abs($url);
+			
+			$this->telegram->sendMessage('warcry', "Опубликована новость:
+<a href=\"{$url}\">{$item->title}</a>");
+		}
+	}
+
+	public function afterDelete($item) {
+		$this->db->deleteTags(Taggable::NEWS, $item->id);
 	}
 }
